4aec412bcd18fc167d9cc0d53bf9f2fdebdab181
Jeffry Gonzalez <jeff@hypertheory.com>
2022-07-12 12:34:07 -0400

    done with practice

--- frontend/registrations-web/cypress/e2e/courses-items.cy.ts
+++ frontend/registrations-web/cypress/e2e/courses-items.cy.ts
@@ -2,31 +2,65 @@ describe('course list items', () => {
   beforeEach(() => {
     cy.intercept('/api/references/courses', {
       fixture: 'course-list-variations.json',
-    });
+    }).as('courses');
 
     cy.intercept('/api/references/offerings', {
       fixture: 'offerings-list-variations.json',
-    });
+    }).as('offerings');
     cy.visit('/courses');
   });
-
   describe('The Common Stuff', () => {
-    it('Shows the Title', () => {});
-    it('Shows the Number of Days', () => {});
-    it('Shows the Description', () => {});
+    beforeEach(() => {
+      cy.wait('@courses');
+      cy.wait('@offerings');
+    });
+    it('Shows the Title', () => {
+      cy.get('[data-test-id="courses-list-item-0"]')
+        .find('[data-test-id="course-item-title"]')
+        .should('contain.text', 'Underwater Basket Weaving');
+    });
+    it('Shows the Number of Days', () => {
+      cy.get('[data-test-id="courses-list-item-0"]')
+        .find('[data-test-id="course-item-days"]')
+        .should('contain.text', '5');
+    });
+    it('Shows the Description', () => {
+      cy.get('[data-test-id="courses-list-item-0"]')
+        .find('[data-test-id="course-item-description"]')
+        .should(
+          'contain.text',
+          'Learn to make baskets underwater. Warning: Moist'
+        );
+    });
   });
   describe('A Course With Offerings', () => {
+    beforeEach(() => {
+      cy.wait('@courses');
+      cy.wait('@offerings');
+    });
     it('The See Offerings Button Is Shown', () => {
-      cy.get('[data-test-id="courses-list-item-0"]').should('exist');
+      cy.get('[data-test-id="courses-list-item-0"]')
+        .find('[data-test-id="course-item-offerings"]')
+        .should('exist');
 
-      // it shows the button, but not the alert
+      cy.get(
+        '[data-test-id="courses-list-item-0"] [data-test-id="course-item-no-offerings]"]'
+      ).should('not.exist');
     });
   });
   describe('A Course Without Offerings', () => {
+    beforeEach(() => {
+      cy.wait('@courses');
+      cy.wait('@offerings');
+    });
     it('The Alert for No Offerings Should Be Shown', () => {
-      cy.get('[data-test-id="courses-list-item-1"]').should('exist');
+      cy.get(
+        '[data-test-id="courses-list-item-1"] [data-test-id="course-item-offerings"]'
+      ).should('not.exist');
 
-      // it shows the alert, but not the button.
+      cy.get('[data-test-id="courses-list-item-1"]')
+        .find('[data-test-id="course-item-no-offerings"]')
+        .should('exist');
     });
   });
 });
